ef00e27a7643234da9b059e4422be5d649623857
Elizabeth2107 <122856988+Elizabeth2107@users.noreply.github.com>
2025-02-12 17:29:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function yesBtn(){
     <img src="images/image-two.gif" class="image-one">
     <p class="card">Yay! You've officially signed up for unlimited love and laughter
     this Valentine's Day. No refunds!😘</p>
-    <h3>I Love You Baby</h3>`
+    <h3>I Love You Baby❤️</h3>`
   //console.log(HTMLbody)
 }
 
@@ -19,7 +19,7 @@ function noBtn(){
   HTMLbody.innerHTML =`<img src="images/image-three.gif" class="image-one">
   <div class="buttonss">
   <button onclick="yesBtn()" class="yes-btn">Yes</button>
-  <button onclick="yesBtn()" class="no-btn2">Pookie? I guess you have no choice now</button>
+  <button onclick="yesBtn()" class="no-btn2">Pookie🥺? I guess you have no choice now🤭</button>
   </div>`
 
 }
